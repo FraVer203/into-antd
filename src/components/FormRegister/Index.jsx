bbf530679bcb06865cdd9c1a132d42ab87b23f53
FraVer203 <FRANCISCO_1213_vera>
2024-05-30 00:11:25 -0600

    feat:(front): válidación de formulario de registro y consumo de endpoint desde vercel

--- src/components/FormRegister/Index.jsx
+++ src/components/FormRegister/Index.jsx
@@ -1,20 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Form, Input, Card } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
 import './FormRegister.css'
 import routes from '/src/routes/routes.js';
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
 
 const FormRegister = () => {
+
+    // Estado de error de registro
+    const [registerError, setRegisterError] = useState(false);
+    // Estado de carga
+    const [loading, setLoading] = useState(false);
+    const [messageError, setMessageError] = useState('')
+    const navigate = useNavigate();
 
     // Función para mostrar los errores en el formulario
     const onFinishFailed = (errorInfo) => {
         console.log('Failed:', errorInfo);
+        setRegisterError(true)
     };
 
     // Función para validar el usuario y contraseña
-    const onFinish = (values) => {
-       console.log('Success: ', values);
+    const onFinish = async (values) => {
+        //console.log('Success: ', values);
+        setLoading(true) // Establecer el estado de carga a true al enviar el formulario
+        try {
+            const response = await axios.post(`${routes.registerBack}users/`, {
+                name: values.name,
+                lastname: values.lastname,
+                email: values.email,
+                password: values.password,
+                roles: ['servicios_escolares']
+            }, {
+                headers: {
+                    'x-access-token': `${routes.token}`
+                }
+            })
+            console.log('Registro exitoso', response.data)
+            navigate('/login')
+        } catch (e) {
+            console.error('Error en el registro: ', e.response.data)
+            setRegisterError(true)
+            setMessageError(e.response.data.message)
+        } finally {
+            setLoading(false)
+        }
     }
+
+    const validatePassword = ({ getFieldValue }) => ({
+        validator(_, value) {
+            if (!value || getFieldValue('password') === value ) {
+                return Promise.resolve()
+            }
+            return Promise.reject(new Error('Las contraseñas no coinciden.'))
+        }
+    })
 
     return(
         <Card
@@ -29,17 +70,45 @@ const FormRegister = () => {
                     remember: true
                 }}
                 onFinish={onFinish}
+                onFinishFailed={onFinishFailed}
             >
                 <Form.Item
-                    name={"username"}
+                    name={"name"}
                     rules={[
                         {
                             required: true,
-                            message: "Por favor ingrese su usuario."
+                            message: "Por favor ingrese su nombre."
                         }
                     ]}
                 >
-                    <Input prefix={<UserOutlined className={"site-form-item-icon"} />} placeholder="Usuario" />
+                    <Input prefix={<UserOutlined className={"site-form-item-icon"} />} placeholder="Nombre" />
+                </Form.Item>
+
+                <Form.Item
+                    name={"lastname"}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Por favor ingrese su apellido."
+                        }
+                    ]}
+                >
+                    <Input prefix={<UserOutlined className={"site-form-item-icon"} />} placeholder="Apellido" />
+                </Form.Item>
+
+
+                <Form.Item
+                    name={"email"}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Por favor ingrese su correo."
+                        }
+                    ]}
+                >
+                    <Input
+                        prefix={<UserOutlined className={"site-form-item-icon"} />} placeholder="Correo electrónico"
+                    />
                 </Form.Item>
 
                 <Form.Item
@@ -58,9 +127,27 @@ const FormRegister = () => {
                     />
                 </Form.Item>
 
+                <Form.Item
+                    name={"password-repet"}
+                    rules={[
+                        {
+                            required: true,
+                            message: "Por favor repita su contraseña."
+                        },
+                        ({ getFieldValue }) => validatePassword({ getFieldValue })
+                    ]}
+                >
+                    <Input
+                        prefix={<LockOutlined className={"site-form-item-icon"} />}
+                        type="password"
+                        placeholder="Repetir Contraseña"
+                    />
+                </Form.Item>
+
                 <Form.Item>
-                    <Button type="primary" htmlType="submit" className={"login-form-button"}>
-                        Registrarse
+                    {registerError && <p style={{ color: 'red' }}>Falló el registro: {messageError} </p>}
+                    <Button type="primary" htmlType="submit" className={"login-form-button"} loading={loading}>
+                        Registrar
                     </Button>
                 </Form.Item>
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,3 +1,6 @@
 const loginRoute = "/login";
 const registerRoute = "/register";
-export default { loginRoute, registerRoute }
+const registerBack = "https://evaluacion-2.vercel.app/api/"
+// const registerBack = "http://localhost:3000/api/"
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1ZTk3NDMzMWM2MTc0MjcyZDY1NDUwZiIsIm5hbWUiOiJrYXJsYSBlcmlrYSIsImxhc3RuYW1lIjoicm9ibGVzIHZhcmdhcyIsImlhdCI6MTcxNzAzOTc3OCwiZXhwIjoxNzE3MTI2MTc4fQ.0aUZvLOZPBDi_S2rKIfZADl2AcisWr1nn0r9KrU_CVI"
+export default { loginRoute, registerRoute, registerBack, token }
