8736c9c808a9e1353f3a5b38821a4beb239c2800
FraVer203 <FRANCISCO_1213_vera>
2024-06-03 23:08:53 -0600

    feat:(front): farreglar errores de las ramas

--- src/components/FormLogin/Index.jsx
+++ src/components/FormLogin/Index.jsx
@@ -1,65 +1,14 @@
 import React, { useState } from 'react';
 import { Button, Form, Input, Card } from 'antd';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
 import './FormLogin.css'
 import routes from '/src/routes/routes.js';
-<<<<<<< HEAD
-import axios from "axios";
-=======
 import authService from '/src/services/auth.js'
 import {useAuth} from "../../hooks/useAuth.js";
->>>>>>> context-auth
 
 const FormLogin = () => {
 
     // Estado para el error del login
-<<<<<<< HEAD
-    const [loginError, setLoginError] = useState(false)
-    // Estado de carga
-    const [loading, setLoading] = useState(false);
-    const [messageError, setMessageError] = useState('')
-    const navigate = useNavigate()
-
-    // Función para mostrar los errores en el formulario
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo);
-        setLoginError(true)
-    };
-
-    // // Función para validar el usuario y contraseña
-    // const onFinish = (values) => {
-    //     const { username, password } = values;
-    //     if (username === user.username && password === user.password) {
-    //         navigate('/')
-    //     } else {
-    //         setLoginError(true);
-    //         const errorInfo = "Credenciales incorrectas. Inténtalo de nuevo."
-    //         onFinishFailed(errorInfo)
-    //     }
-    // }
-
-    // Función para validar el usuario y contraseña
-    const onFinish = async (values) => {
-        //console.log('Success: ', values);
-        setLoading(true) // Establecer el estado de carga a true al enviar el formulario
-        try {
-            const response = await axios.post(`${routes.registerBack}auth/signin/`, {
-                email: values.email,
-                password: values.password,
-            })
-            console.log('Registro exitoso', response.data)
-            navigate('/')
-        } catch (e) {
-            console.error('Error en el inicio: ', e.response.data)
-            setLoginError(true)
-            setMessageError(e.response.data.message)
-        } finally {
-            setLoading(false)
-        }
-    }
-
-=======
     const [loginError, setLoginError] = useState(false);
     const [loading, setLoading] = useState(false);
     // const navigate = useNavigate();
@@ -94,14 +43,13 @@ const FormLogin = () => {
         }
     }
 
->>>>>>> context-auth
     return(
         <Card
             tittle="Bienvenido de nuevo"
             bordered={false}
             className="responsive-card"
         >
-            {loginError && <p style={{ color: 'red' }}>Error: {messageError} </p>}
+            {loginError && <p style={{ color: 'red' }}>Credenciales incorrectas. Inténtalo de nuevo.</p>}
             <Form
                 name={"normal_login"}
                 className={"login_form"}
@@ -109,7 +57,6 @@ const FormLogin = () => {
                     remember: true
                 }}
                 onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
             >
                 <Form.Item
                     name={"email"}
@@ -142,7 +89,7 @@ const FormLogin = () => {
                 </Form.Item>
 
                 <Form.Item>
-                    <Button type="primary" htmlType="submit" className={"login-form-button"} loading={loading}>
+                    <Button type="primary" htmlType="submit" className={"login-form-button"}>
                         Iniciar Sesión
                     </Button>
                 </Form.Item>
