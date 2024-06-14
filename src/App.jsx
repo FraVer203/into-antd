a1cb43955fefc9799159d80c4b23ae9a7f5b1582
JOSE MARIA DE JESUS RICO <2021371064@uteq.edu.mx>
2024-06-14 00:39:28 -0600

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import AppRoutes from "./routes/Index.jsx";
 import {BrowserRouter} from "react-router-dom";
 import { AuthProvider } from "../src/context/AuthContext.jsx";
 
-
 function App() {
 
     return (
